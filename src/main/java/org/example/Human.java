fbff820b936df36f2e265811c4a37c71d0559532
krasnoperov <runyugin@mail.ru>
2024-02-12 11:55:58 +0300

    lesson2

---
+++ src/main/java/org/example/Human.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Human extends Players implements Skill{
+
+    private String name;
+    private int heigh_jamp;
+    private int run_distance;
+
+    public Human(String name, int heigh_jamp, int run_distance) {
+        this.name = name;
+        this.heigh_jamp = heigh_jamp;
+        this.run_distance = run_distance;
+    }
+
+    @Override
+    public void jamp() {
+        System.out.println("Прыгаем");
+    }
+
+    public int getHeigh_jamp() {
+        return heigh_jamp;
+    }
+
+    public int getRun_distance() {
+        return run_distance;
+    }
+
+    public void run() {
+        System.out.println("Бежим");
+    }
+
+}
---
+++ src/main/java/org/example/RunRoad.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class RunRoad extends Barrier{
+
+    private int distance;
+
+    public RunRoad(int distance) {
+        this.distance = distance;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
